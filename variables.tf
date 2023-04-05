3f1ec0724fd23f2cfbd9e0d80521f77a100883bf
Rajeev Jaggavarapu <rajiv.nix@gmail.com>
2023-04-05 12:31:17 +0530

    Updating default NSG vaule to null

--- variables.tf
+++ variables.tf
@@ -75,4 +75,5 @@ variable "associate_nsg" {
 variable "network_security_group_id" {
   type        = string
   description = "(Required) The ID of the Network Security Group which should be associated with the Subnet. Changing this forces a new resource to be created."
+  default     = null
 }
